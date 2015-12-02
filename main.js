746cb3940d5f5b2e5665e75545f37288c4178ff6
kcypher13 <cypher@ftad.fr>
2015-12-02 16:31:53 +0100

    test geoloc

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var watchID = navigator.geolocation.watchPosition(function(position) {
         console.log(position);
-        $('.latitude').text(position.coords.latitude);
+        $('.latitude span').text(position.coords.latitude);
+        $('.longitude span').text(position.coords.longitude);
+        $('.speed span').text(position.coords.speed);
+        $('.heading span').text(position.coords.heading);
+        $('.accuracy span').text(position.coords.accuracy);
     }
 );
